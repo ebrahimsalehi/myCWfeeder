c13df839b38c2ea733b86b616c9f9bd0c8ba75ab
Ebrahim Salehi <ebrahim.salehi@boku.com>
2020-08-23 22:15:38 +0200

    Add and endpoint to put metrics in Cloudwatch

--- src/main/java/com/aws/codestar/projecttemplates/controller/HelloWorldController.java
+++ src/main/java/com/aws/codestar/projecttemplates/controller/HelloWorldController.java
@@ -1,6 +1,14 @@
 package com.aws.codestar.projecttemplates.controller;
 
+import com.amazonaws.services.cloudwatch.AmazonCloudWatch;
+import com.amazonaws.services.cloudwatch.AmazonCloudWatchClientBuilder;
+import com.amazonaws.services.cloudwatch.model.Dimension;
+import com.amazonaws.services.cloudwatch.model.MetricDatum;
+import com.amazonaws.services.cloudwatch.model.PutMetricDataRequest;
+import com.amazonaws.services.cloudwatch.model.PutMetricDataResult;
+import com.amazonaws.services.cloudwatch.model.StandardUnit;
 import org.json.JSONObject;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -24,6 +32,38 @@ public class HelloWorldController {
     @RequestMapping(method = RequestMethod.POST, produces = "application/json")
     public ResponseEntity helloWorldPost(@RequestParam(value = "name", defaultValue = "World") String name) {
         return ResponseEntity.ok(createResponse(name));
+    }
+
+    @RequestMapping(method = RequestMethod.GET, produces = "application/json", path = "metrics")
+    public ResponseEntity putMetrics(@RequestParam(value = "val", defaultValue = "0.0") Double val) {
+        putMetricsToCW(val);
+        return ResponseEntity.ok("metrics put successfully");
+    }
+
+    private void putMetricsToCW(Double value) {
+        final AmazonCloudWatch cw =
+                AmazonCloudWatchClientBuilder.defaultClient();
+
+        Dimension dimension = new Dimension()
+                .withName("UNIQUE_PAGES")
+                .withValue("URLS");
+
+        System.out.println("sending metrics ");
+
+        MetricDatum datum = new MetricDatum()
+                .withMetricName("PAGES_VISITED")
+                .withUnit(StandardUnit.None)
+                .withValue(value)
+                .withDimensions(dimension);
+
+        PutMetricDataRequest request = new PutMetricDataRequest()
+                .withNamespace("SITE/TRAFFIC")
+                .withMetricData(datum);
+
+        PutMetricDataResult response = cw.putMetricData(request);
+        System.out.println(response);
+
+
     }
 
     private String createResponse(String name) {
